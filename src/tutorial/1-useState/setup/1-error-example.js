ce399b1b0f6bfb859d52cf585045a5d84e9cd1bd
ghintema <harm.intemann@gmail.com>
2023-01-19 19:20:51 -0400

    End of day

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+
+  const [title, setTitle] = useState('first title')
+
+  const titles = ['first title', 
+                  'second title',
+                  'third title',
+                  'yet another title',
+                  'and one final last title',
+                  'no, there is just one more']
+
+  const handleClick = () => { 
+    // const rand = Math.floor(Math.random()*6);
+    // setTitle(titles[rand]);
+
+    if( title ==='first title') {
+      setTitle('second title')
+    } else {
+      setTitle('first title');
+    }
+    console.log(title) // attention: This logs the OLD state for title. NOT the new one.
+  }
+  
+  console.log('render')
+  return (
+    <>
+      <h2>{title}</h2>
+      <button type="button" onClick={handleClick} className="btn">ClickMe</button>
+    </>
+  );
 };
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,31 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [names, setNames] = React.useState(data);
+
+
+  const deletePerson = (id) => { 
+    // const id = e.target.parentNode.id;
+    const namesCopy = [...names];
+    const newNames = namesCopy.filter(name => name.id != id)
+    setNames(newNames);
+    console.log('newNames', newNames)
+  }
+
+  return (
+    <>
+      {names.map((person, index) => {
+        const { id, name } = person;
+        return (
+          <div key = {id} id={id} className="item" >
+            <h4>{name}</h4>
+            <button onClick={() => deletePerson(id)}>löschen</button>
+          </div>  
+        )
+      })}
+      <button onClick={() => setNames(data)}>Restore the list</button>
+    </>
+    )
 };
 
 export default UseStateArray;
